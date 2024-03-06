c27c842bbe58391e977055dc6a60c81ee0f347db
Pravin Mane <141111800+Pravin7639@users.noreply.github.com>
2024-03-06 16:45:30 +0530

    ADO.NET Core App

---
+++ ADONETCoreApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using ADONETCoreApp;
+
+Console.WriteLine("*** ALL TRAINERS ***");
+
+TrainerDB db = new TrainerDB();
+var trainers = db.AllTrainers();
+
+foreach (var item in trainers)
+{
+    Console.WriteLine($"Id : {item.Id} Name : {item.Name} City : {item.City}" +
+        $" Experience : {item.Experience}");
+}
+
+
+Console.WriteLine("Create A New Trainer");
+Console.WriteLine("Please enter name");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter city");
+string city = Console.ReadLine();
+Console.WriteLine("Please enter experience");
+int experience = int.Parse(Console.ReadLine());
+
+Trainer t = new Trainer() { Name = name, City = city, Experience = experience };
+
+int rn;
+if (db.CreateTrainer(t, out rn))
+{
+    Console.WriteLine($"Trainer Inserted Successfully. Trainer Id is {rn}");
+}
+else
+{
+    Console.WriteLine("Failed to insert new trainer");
+}
+
+
+
+Console.WriteLine("Enter trainer id to delete");
+int id = int.Parse(Console.ReadLine());
+
+bool status = db.DeleteTrainer(id);
+if (status)
+{
+    Console.WriteLine($"Trainer with Id {id} Deleted Successfully.");
+}
+else
+{
+    Console.WriteLine($"Trainer with Id {id} Delete Failed.");
+}
+
+
+Console.ReadLine();
+
+
---
+++ ADONETCoreApp/TrainerDB.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ADONETCoreApp
+{
+    public class TrainerDB
+    {
+        public List<Trainer> AllTrainers()
+        {
+
+            List<Trainer> trainers = new List<Trainer>();
+
+            string connectionString =
+                "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;encrypt=false;";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("select * from Trainer", con); // inline query oR ADHOC query
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Trainer t = new Trainer()
+                    {
+                        Id = (int)reader["Id"],
+                        Name = reader["Name"].ToString(),
+                        City = reader["City"].ToString(),
+                        Experience = (int)reader["Experience"]
+                    };
+                    trainers.Add(t);
+
+                }
+            }
+            else
+            {
+                trainers = null;
+            }
+            con.Close();
+            return trainers;
+
+        }
+
+
+        public bool DeleteTrainer(int id)
+        {
+            string connectionString =
+                "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;encrypt=false;";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("uspDeleteTrainer", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            SqlParameter status = new SqlParameter()
+            {
+                ParameterName = "@Status",
+                SqlDbType = SqlDbType.Bit,
+                Direction = ParameterDirection.Output,
+            };
+
+            cmd.Parameters.Add(status);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            return (bool)status.Value;
+        }
+
+
+        public bool CreateTrainer(Trainer trainer, out int rollNumber)
+        {
+            string connectionString =
+                "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;encrypt=false;";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("uspCreateTrainer", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Name", trainer.Name);
+            cmd.Parameters.AddWithValue("@City", trainer.City);
+            cmd.Parameters.AddWithValue("@Experience", trainer.Experience);
+
+            SqlParameter rn = new SqlParameter()
+            {
+                ParameterName = "@RollNumber",
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Output
+            };
+            cmd.Parameters.Add(rn);
+
+            SqlParameter status = new SqlParameter()
+            {
+                ParameterName = "@Status",
+                SqlDbType = SqlDbType.Bit,
+                Direction = ParameterDirection.Output
+            };
+            cmd.Parameters.Add(status);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            rollNumber = (int)rn.Value;
+
+            return (bool)status.Value;
+        }
+
+    }
+}
