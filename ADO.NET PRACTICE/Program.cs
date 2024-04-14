16157672276333f2ee0cee728c2de87ffef3f244
Pravin Mane <141111800+Pravin7639@users.noreply.github.com>
2024-04-14 19:15:49 +0530

    Fetch Data From Student / student and Trainer

---
+++ ADO.NET PRACTICE/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_PRACTICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Fetching student data from DataBase
+
+            //StudentDB db = new StudentDB();
+            //List<Student> s = db.AllStudent();
+
+            //Console.WriteLine("All Students From Database");
+            //foreach (var item in s)
+            //{
+            //    Console.WriteLine($"RollNumber : {item.RollNumber} Name : {item.Name} " +
+            //        $"Gender : {item.Gender} TrainerId : {item.TrainerId} ");
+            //}
+
+            #endregion Fetching student AND Trainer data from DataBase
+
+
+            #region Fetching student AND Trainer data from DataBase
+
+
+            //StudentDB db = new StudentDB();
+
+            //List<Student> students;
+            //List<Trainer> trainers;
+
+            //db.AllStudentTrainer(out students, out trainers);
+
+            //Console.WriteLine("All Students From DataBase");
+            //foreach (var item in students)
+            //{
+            //    Console.WriteLine($"RollNumber : {item.RollNumber} Name : {item.Name} Gender : {item.Gender} " +
+            //        $"TrainerId : {item.TrainerId} ");
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+
+            //Console.WriteLine("All Trainers From DataBase");
+            //foreach (var item in trainers)
+            //{
+            //    Console.WriteLine($"Id : {item.Id} Name : {item.Name} City : {item.City} " +
+            //        $"Experince : {item.Experience} ");
+            //}
+
+
+            #endregion Fetching student AND Trainer data from DataBase
+
+
+            #region GetStudentByRollNumber
+
+            //StudentDB db = new StudentDB();
+
+            //Console.WriteLine("Please Enter Student RollNumber");
+            //int RollNumber = int.Parse(Console.ReadLine());
+
+            //Student s = db.GetStudentByRollNumber(RollNumber);
+
+            //if (s != null)
+            //{
+            //    Console.WriteLine($"RollNumber : {s.RollNumber} Name : {s.Name} Gender : {s.Gender} " +
+            //        $"TrainerId : {s.TrainerId} ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Student with RollNumber {RollNumber} is not Found");
+            //}
+
+            #endregion GetStudentByRollNumber
+
+
+            #region SQL Injection
+
+            //Console.WriteLine("Please Enter A RollNumber To Search Stuent");
+
+            //string rn = Console.ReadLine();
+
+            //string ConnectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;";
+
+            //SqlConnection con = new SqlConnection(ConnectionString);
+
+            //string cmdText =$"Select * from Student where RollNumber= {rn}";
+
+            //SqlCommand cmd = new SqlCommand(cmdText, con);
+            //con.Open();
+
+            //SqlDataReader reader = cmd.ExecuteReader();
+
+            //if (reader.HasRows)
+            //{
+            //    while (reader.Read())
+            //    {
+            //        Student s = new Student()
+            //        {
+            //            RollNumber= (int)reader["RollNumber"],
+            //            Name=reader["Name"].ToString(),
+            //            Gender=reader["Gender"].ToString(),
+            //            TrainerId=(int) reader["TrainerId"]
+            //        };
+
+            //        Console.WriteLine($"RollNumber : {s.RollNumber} Name : {s.Name} Gender : {s.Gender} " +
+            //            $"TrainerId : {s.TrainerId} ");
+
+            //        break;
+
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Student With RollNumber {rn} is Not Found");
+            //}
+
+
+
+            #endregion SQL Injection
+
+
+
+            //Console.WriteLine("Create A New Student");
+            //Console.WriteLine("Please enter Name ");
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ADO.NET PRACTICE/StudentDB.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_PRACTICE
+{
+    public class StudentDB
+    {
+
+        #region Fetching student data from DataBase
+
+        //public List<Student> AllStudent()
+        //{
+        //    List<Student> students = new List<Student>();
+
+        //string ConnectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true";
+
+        //SqlConnection con = new SqlConnection(ConnectionString);
+
+        //    string cmdText = "Select * from Student";
+
+        //    SqlCommand cmd = new SqlCommand(cmdText, con);
+
+        //    con.Open();
+
+        //    SqlDataReader reader = cmd.ExecuteReader();
+
+        //    if (reader.HasRows)
+        //    {
+        //        while (reader.Read())
+        //        {
+        //            Student s = new Student()
+        //            {
+        //                RollNumber = (int)reader["RollNumber"],
+        //                Name = reader["Name"].ToString(),
+        //                Gender = reader["Gender"].ToString(),
+        //                TrainerId = (int)reader["TrainerId"]
+        //            };
+        //            students.Add(s);
+
+        //        }
+        //    }
+
+        //    con.Close();
+        //    return students;
+
+        //}
+
+
+
+
+
+        #endregion Fetching student data from DataBase
+
+
+
+        #region Fetching student AND Trainer data from DataBase
+
+        //public void AllStudentTrainer(out List<Student> students, out List<Trainer> trainers)
+        //{
+        //    students = new List<Student>();
+        //    trainers = new List<Trainer>();
+
+
+        //    string connectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true";
+
+        //    SqlConnection con = new SqlConnection(connectionString);
+
+        //    string cmdText = "Select * from Student; Select * from Trainer";
+
+        //    SqlCommand cmd = new SqlCommand(cmdText, con);
+
+        //    con.Open();
+
+        //    SqlDataReader reader = cmd.ExecuteReader();
+
+        //    if (reader.HasRows)
+        //    {
+        //        while (reader.Read())
+        //        {
+        //            Student s = new Student()
+        //            {
+        //                RollNumber = (int) reader["RollNumber"],
+        //                Name=reader["Name"].ToString(),
+        //                Gender=reader["Gender"].ToString(),
+        //                TrainerId=(int) reader["TrainerId"]
+        //            };
+        //            students.Add(s);
+        //        }
+        //    }
+        //    reader.NextResult();
+
+        //    if (reader.HasRows)
+        //    {
+        //        while (reader.Read())
+        //        {
+        //            Trainer t = new Trainer()
+        //            {
+        //                Id = (int)reader["Id"],
+        //                Name = reader["Name"].ToString(),
+        //               City=reader["City"].ToString(),
+        //                Experience = (int) reader["Experience"]
+        //            };
+
+        //            trainers.Add(t);
+        //        }
+        //    }
+
+        //    con.Close();
+
+        //}
+
+
+        #endregion Fetching student AND Trainer data from DataBase
+
+
+        #region GetStudentByRollNumber
+
+        //public Student GetStudentByRollNumber(int id)
+        //{
+        //    Student student = new Student();
+
+        //    string connectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true";
+
+        //    SqlConnection con = new SqlConnection(connectionString);
+
+        //    string cmdText =$"Select * from Student where RollNumber={id}";
+
+        //    SqlCommand cmd = new SqlCommand(cmdText, con);
+
+        //    con.Open();
+
+        //    SqlDataReader reader = cmd.ExecuteReader();
+
+        //    while (reader.Read())
+        //    {
+        //        student.RollNumber = (int)reader["RollNumber"];
+        //        student.Name = reader["Name"].ToString();
+        //        student.Gender = reader["Gender"].ToString();
+        //        student.TrainerId = (int)reader["TrainerId"];
+
+        //        break;
+        //    }
+
+        //    con.Close();
+
+        //    return student;
+        //}
+        #endregion GetStudentByRollNumber
+
+
+
+
+
+
+
+
+
+
+    }
+}
